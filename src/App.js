a34d2fd87bc5e61deefc32b02a430794b26c9f49
RachelyLevy <rl304090100@gmail.com>
2021-12-26 18:14:31 +0200

    creating a backGround component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import BackGround from './BackGround';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
-    </div>
+   <>
+       <BackGround></BackGround>
+     
+     </>
   );
 }
 
---
+++ src/BackGround.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+export default function BackGround() {
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Choose A BackGround </FormLabel>
+      <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
+        <FormControlLabel value="BackGround1" control={<Radio />} label="BackGround 1" />
+        <FormControlLabel value="BackGround2" control={<Radio />} label="BackGround 2" />
+        <FormControlLabel value="BackGround3" control={<Radio />} label="BackGround 3" />
+        
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+
+
