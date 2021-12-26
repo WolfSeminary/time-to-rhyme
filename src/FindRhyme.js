bfe09a81f0569629c9f07b11df11d7fec2598879
efrat2-cohen <e0583231267@gmail.com>
2021-12-26 15:08:39 +0200

    createNewInput

--- src/FindRhyme.js
+++ src/FindRhyme.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function FindRhyme() {
+export default function CreateButton() {
   return (
     <Stack spacing={2} direction="row">
      
