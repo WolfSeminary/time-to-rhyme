98bf67a6d21b50642d720cde31f343e7d58903d9
efrat2-cohen <e0583231267@gmail.com>
2021-12-26 14:09:20 +0200

    hhur

---
+++ src/FindRhyme.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function FindRhyme() {
+  return (
+    <Stack spacing={2} direction="row">
+     
+      <Button variant="contained">Find me a Rhyme</Button>
+
+    </Stack>
+  );
+}
