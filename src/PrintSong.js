60817dc31ea795e8723bb37083e0ffd38f0c475e
chaniapafueffer <c0534165825@gmail.com>
2021-12-26 14:45:46 +0200

    createButtonToPrintSong

---
+++ src/PrintSong.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+
+
+export default function PrintSong() {
+    return (
+      <Stack spacing={2} direction="row">
+        
+        <Button variant="contained">Print Designed Song</Button>
+       
+      </Stack>
+    );
+  }
