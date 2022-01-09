6aee39c4af04fd47149cc14c6a4afb90ea752b17
hitmachut <hitmachut@students.local>
2022-01-09 12:39:55 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import WordToRhyme from './WordToRhyme';
-
 function App() {
   return (
     <div className="App">
-
     </div>
   );
 }
-
 export default App;
--- src/WordToRhyme.js
+++ src/WordToRhyme.js
@@ -1,25 +1,24 @@
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
-const WordToRhyme=()=>{
-const [wordToRhyme , setWordToRhyme] = useState();
-const onWordToRhymeChange=(event)=>{
-  setWordToRhyme(event.target.value);
-}
-return(
+const WordToRhyme = () => {
+  const [wordToRhyme, setWordToRhyme] = useState();
+  const onWordToRhymeChange = (event) => {
+    setWordToRhyme(event.target.value);
+  }
+  return (
     <>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-    <TextField id="outlined-basic" label="Word to Rhyme" variant="outlined" onChange = {(event) => onWordToRhymeChange(event)}/>
-    </Box>
-
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Word to Rhyme" variant="outlined" onChange={(event) => onWordToRhymeChange(event)} />
+      </Box>
     </>
-)
+  )
 }
 export default WordToRhyme;
