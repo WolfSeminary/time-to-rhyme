20ced7d969542ad3af6b1c8445158faf319a53f5
hitmachut <hitmachut@students.local>
2022-01-16 13:51:32 +0200

    addThankYouComponent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ThankYouModal from './ThankYouModal';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
     </div>
   );
 }
-
 export default App;
