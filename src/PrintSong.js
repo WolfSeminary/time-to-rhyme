7b9763ba47112b7fa2d25435e47d314f5fce7970
chaniPfueffer <c0534165825@gmail.com>
2022-01-16 13:52:41 +0200

    createTheShouldShowThankYouModalState

--- src/PrintSong.js
+++ src/PrintSong.js
@@ -3,9 +3,15 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function PrintSong() {
+  const [shouldShowThankYouModal, setShouldShowThankYouModal] = useState(false);
+  const o = () => {
+    return (
+      shouldShowThankYouModal == true && < ThankYouModal />
+    )
+  }
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="contained">Print Designed Song</Button>
+      <Button variant="contained" onclick={o}>Print Designed Song</Button>
     </Stack>
   );
 }
