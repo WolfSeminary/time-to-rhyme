9eb639b0b1a3197f4b239b913918c2007b2716fc
EsterNachum <esternachum@gmail.com>
2022-01-16 18:23:11 +0200

    Allows the user to choose a specific background

--- src/BackGround.js
+++ src/BackGround.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -6,17 +6,22 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 
 export default function BackGround() {
+  const [selectedBackground, setSelectedBackground] = useState("");
+  const onBackgroundChange = (e) => {
+    setSelectedBackground(e.target.value);
+  };
   return (
     <FormControl component="fieldset">
       <FormLabel component="legend">Choose A BackGround </FormLabel>
       <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
-        <FormControlLabel value="BackGround1" control={<Radio />} label="BackGround 1" />
-        <FormControlLabel value="BackGround2" control={<Radio />} label="BackGround 2" />
-        <FormControlLabel value="BackGround3" control={<Radio />} label="BackGround 3" />
+        <FormControlLabel value="BackGround1" control={<Radio />} label="BackGround 1" onChange={onBackgroundChange} />
+        <FormControlLabel value="BackGround2" control={<Radio />} label="BackGround 2" onChange={onBackgroundChange} />
+        <FormControlLabel value="BackGround3" control={<Radio />} label="BackGround 3" onChange={onBackgroundChange} />
       </RadioGroup>
     </FormControl>
   );
 }
+
 
 
 
