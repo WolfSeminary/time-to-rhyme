695661d7b5699f95e00dbe575670f29e16e67e5b
dassy-f <df0556769530@gmail.com>
2022-01-16 12:26:22 +0200

    feat:(clearSong: Add a ClearSong button component)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import RhymesList from './RhymesList';
+import ClearSong from './ClearSong'
 import React from 'react'
 
 function App() {
   return (
     <div className="App">
-     <RhymesList/>
+     <ClearSong/>
     </div>
   );
 }
