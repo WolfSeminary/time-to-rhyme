e5e79dc8d78ffd27e9bede707cdc36ec512535ed
RachelyLevy <rl304090100@gmail.com>
2022-01-16 13:23:56 +0200

    managed the song state in write-song component

--- src/WriteSong.js
+++ src/WriteSong.js
@@ -2,26 +2,33 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { red } from '@mui/material/colors';
 import Box from '@mui/material/Box';
-
-const WriteSong=()=>{
-    return(<>
-     <Box
+import { useState } from 'react';
+const WriteSong = () => {
+  const [song, setSong] = useState();
+  const onWriteSong = (params) => {
+    setSong( params);
+     }
+  return (<>
+    <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '35ch'},
+        '& .MuiTextField-root': { m: 1, width: '35ch' },
       }}
       noValidate
       autoComplete="off"
-    > 
- <TextField  
-    variant="outlined"
-     style = {{width:600}}
-     label="Write Song"
-     defaultValue="Write the Song"
-     multiline rows={8}
-     fullWidth
-  /></Box> 
-</>
-    )
+    >
+      <TextField
+        variant="outlined"
+        style={{ width: 600 }}
+        label="Write Song"
+        defaultValue="Write the Song"
+        multiline rows={8}
+        value={song}
+        fullWidth
+        onChange={(e) => onWriteSong(e.target.value)}
+      />
+    </Box>
+  </>
+  )
 }
 export default WriteSong
