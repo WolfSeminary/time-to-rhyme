43826d9caa0111561cf26bd102591eccadfaa340
tamar-frid <tf2002565@gmail.com>
2022-01-16 12:52:20 +0200

    Manage State

--- src/FetchMoreRhymes.js
+++ src/FetchMoreRhymes.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import   React from 'react';
 import { useState } from 'react';
 
-const FetchMoreRhymes = () => {
-  const [currentRhymesRange, setcurrentRhymesRange] = useState(0)
-  function fetchRhymes() {
-    setcurrentRhymesRange(currentRhymesRange + 10)
-  }
+const FetchMoreRhymes=()=>{
+const[currentRhymesRange, setCurrentRhymesRange]=useState(0)
+
+function fetchRhymes(){
+  setcurrentRhymesRange(currentRhymesRange+10)
+ }
 
   return <div>here has to be a button</div>
 }
