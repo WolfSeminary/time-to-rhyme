0856f2efb1e33bdb6e10860d034530e21cf24d7d
Rachely-Braverman <rachelb55677@gmail.com>
2022-01-16 16:35:46 +0200

    listrhymes

--- src/NumOfSyllables.js
+++
@@ -1,8 +0,0 @@
-import {TextField} from '@mui/material'
-
-const NumOfSyllables  = () => {
-    return(
-        <TextField id="outlined-basic" label="Number of Syllables" variant="outlined" />
-    )
-}
-export default NumOfSyllables ;
