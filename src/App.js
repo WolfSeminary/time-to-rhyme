b909333d109c4df0b5776c580609ca7aea144505
hitmachut <hitmachut@students.local>
2022-01-18 16:01:02 +0200

    finally

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import ThankYouModal from './ThankYouModal';
+
 function App() {
   return (
     <div className="App">
+     <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!
+        </p>
+      </header>
     </div>
   );
 }
+
 export default App;
