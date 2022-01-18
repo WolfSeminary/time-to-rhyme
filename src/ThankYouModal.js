877203c27018474fccf9715c821368539f028594
hitmachut <hitmachut@students.local>
2022-01-18 15:50:53 +0200

    changes

--- src/ThankYouModal.js
+++ src/ThankYouModal.js
@@ -3,56 +3,55 @@ import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 const ThankYouModal = () => {
-  {
-        const rootRef = React.useRef(null);
-        return (
-            <Box
+    const rootRef = React.useRef(null);
+    return (
+        <Box
+            sx={{
+                height: 300,
+                flexGrow: 1,
+                minWidth: 300,
+                transform: 'translateZ(0)',
+                '@media all and (-ms-high-contrast: none)': {
+                    display: 'none',
+                },
+            }}
+            ref={rootRef}
+        >
+            <Modal
+                disablePortal
+                disableEnforceFocus
+                disableAutoFocus
+                open
+                aria-labelledby="server-modal-title"
+                aria-describedby="server-modal-description"
                 sx={{
-                    height: 300,
-                    flexGrow: 1,
-                    minWidth: 300,
-                    transform: 'translateZ(0)',
-                    '@media all and (-ms-high-contrast: none)': {
-                        display: 'none',
-                    },
+                    display: 'flex',
+                    p: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
                 }}
-                ref={rootRef}
+                container={() => rootRef.current}
             >
-                <Modal
-                    disablePortal
-                    disableEnforceFocus
-                    disableAutoFocus
-                    open
-                    aria-labelledby="server-modal-title"
-                    aria-describedby="server-modal-description"
+                <Box
                     sx={{
-                        display: 'flex',
-                        p: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
+                        position: 'relative',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        border: '2px solid #000',
+                        boxShadow: (theme) => theme.shadows[5],
+                        p: 4,
                     }}
-                    container={() => rootRef.current}
                 >
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            width: 400,
-                            bgcolor: 'background.paper',
-                            border: '2px solid #000',
-                            boxShadow: (theme) => theme.shadows[5],
-                            p: 4,
-                        }}
-                    >
-                        <Typography id="server-modal-title" variant="h6" component="h2">
-                         Thank You!
-                        </Typography>
-                        <Typography id="server-modal-description" sx={{ pt: 2 }}>
+                    <Typography id="server-modal-title" variant="h6" component="h2">
+                        Thank You!
+                    </Typography>
+                    <Typography id="server-modal-description" sx={{ pt: 2 }}>
                         Thank you for using RhymeTime for writing your song, looking forward to seeing you again!
-                        </Typography>
-                    </Box>
-                </Modal>
-            </Box>
-        );
-    }
+                    </Typography>
+                </Box>
+            </Modal>
+        </Box>
+    );
+}
 }
 export default ThankYouModal;
