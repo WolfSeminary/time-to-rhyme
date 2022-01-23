69439d22f0b77936cc362d03e6da555840fd9936
dassy-f <df0556769530@gmail.com>
2022-01-23 12:18:02 +0200

    ddd

--- src/PrintSong.js
+++ src/PrintSong.js
@@ -3,20 +3,21 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function PrintSong() {
-const [shouldShowThankYouModal,setShouldShowThankYouModal]=useState(false); 
-const thank=()=>{
-  return (
-    shouldShowThankYouModal==true&&< ThankYouModal/>
-  )
-} 
-function onPrintSongClick(){
-  setShouldShowThankYouModal(true);
-  window.print();
-}
+  
+  const [shouldShowThankYouModal, setShouldShowThankYouModal] = useState(false);
+  const thank = () => {
+    return (
+      shouldShowThankYouModal == true && < ThankYouModal />
+    )
+  }
+  function onPrintSongClick() {
+    setShouldShowThankYouModal(true);
+    window.print();
+  }
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="contained" onchange={onPrintSongClick} onclick={thank}
-    >Print Designed Song</Button>
+      <Button variant="contained" onchange={onPrintSongClick} onclick={thank} >
+        Print Designed Song</Button>
     </Stack>
   );
 }
