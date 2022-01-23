80468878f627e78d88f4b310bea4081caafdd390
efrat2-cohen <e0583231267@gmail.com>
2022-01-23 12:32:08 +0200

    Aaddonclickevent

--- src/FindRhyme.js
+++ src/FindRhyme.js
@@ -1,13 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import FetchRhymes from './FetchRhymes'
 
 export default function FindRhymeBtn() {
   return (
-    <Stack spacing={2} direction="row">
-     
+    <Stack onClick={FetchRhymes} spacing={2} direction="row">
       <Button variant="contained">Find me a Rhyme</Button>
-
     </Stack>
   );
 }
