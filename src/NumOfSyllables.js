2aee47e9d4ad89970e85eb7dbc9446163d855173
efrat2-cohen <e0583231267@gmail.com>
2022-01-23 16:39:19 +0200

    take the input from the user

--- src/NumOfSyllables.js
+++ src/NumOfSyllables.js
@@ -1,8 +1,14 @@
-import {TextField} from '@mui/material'
+import {TextField} from '@mui/material';
+import { useState } from 'react';
 
-const NumOfSyllables  = () => {
+const NumOfSyllables=()=>{
+    const[numOfSyllables, setNumOfSyllables]=useState(0)
+     const onNumOfSyllablesChange=(e)=>{
+         setNumOfSyllables(e.target.value);
+         console.log(numOfSyllables);
+     }
     return(
-        <TextField id="outlined-basic" label="Number of Syllables" variant="outlined" />
+        <TextField onChange={e=>onNumOfSyllablesChange(e)} id="outlined-basic" label="Number of Syllables" variant="outlined" />
     )
 }
 export default NumOfSyllables ;
