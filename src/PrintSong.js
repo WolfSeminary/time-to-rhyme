5d86354d7160fd703dcc8f66a7a14e2cfad333a4
RachelyLevy <rl304090100@gmail.com>
2022-01-23 18:07:16 +0200

    Handle Close to ' thank you model'

--- src/PrintSong.js
+++ src/PrintSong.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import ThankYouModal from './ThankYouModal'
 
 export default function PrintSong() {
-const [shouldShowThankYouModal,setShouldShowThankYouModal]=useState(false); 
-const thank=()=>{
+  const [shouldShowThankYouModal, setShouldShowThankYouModal] = useState(false);
+  const thank = () => {
+    setShouldShowThankYouModal(true);
+  }
   return (
-    shouldShowThankYouModal==true&&< ThankYouModal/>
+    <>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" onClick={thank}
+        >Print Designed Song</Button>
+      </Stack>
+      {
+        shouldShowThankYouModal ? <ThankYouModal setShouldShowThankYouModal={setShouldShowThankYouModal} /> : <></>
+      }
+    </>
   )
-} 
-  return (
-    <Stack spacing={2} direction="row">
-      <Button variant="contained" onclick={thank}
-    >Print Designed Song</Button>
-    </Stack>
-  );
 }
