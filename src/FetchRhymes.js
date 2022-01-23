e5804382df0dcbf5d6a498c1080ddb87d9a2e32e
tamar-frid <96005551+tamar-frid@users.noreply.github.com>
2022-01-23 16:36:31 +0200

    feat(FetchRhymes):get rhymes (#47)
    * listrhymes
    
    * getlistrhymes
    
    * getlistrhymes
    
    * getlistrhymes
    
    Co-authored-by: Rachely-Braverman <rachelb55677@gmail.com>

--- src/FetchRhymes.js
+++ src/FetchRhymes.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function FindRhymeBtn() {
+export default function FindRhyme() {
+  const [wordToRhyme, setWordToRhyme] = useState()
+  const FetchRhymes = () => {
+  let word=wordToRhyme
+   fetch('https://rhymebrain.com/talk?function=getRhymes&word=' + word).then(res => res.json())
+  }
   return (
     <Stack spacing={2} direction="row">
-     
       <Button variant="contained">Find me a Rhyme</Button>
-
     </Stack>
   );
 }
