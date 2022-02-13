d1745182b85fb6418a19a485575a7537713b3df6
esti-choen <st0556778168@gmail.com>
2022-02-13 14:09:25 +0200

    fix

--- src/AppBar.js
+++ src/AppBar.js
@@ -13,18 +13,9 @@ export default function ButtonAppBar() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                        Time to rhyme
                     </Typography>
-                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </Box>
