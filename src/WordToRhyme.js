ce85c54ed53fab781ffa14995d7aa77ed4710608
hitmachut <hitmachut@students.local>
2022-02-13 16:06:16 +0200

    moveTheState

--- src/WordToRhyme.js
+++ src/WordToRhyme.js
@@ -1,21 +1,23 @@
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-const WordToRhyme=()=>{
-return(
+import { useState } from 'react';
+const WordToRhyme = ({setWordToRhyme}) => {
+    const onWordToRhymeChange = (event) => {
+    setWordToRhyme(event.target.value);
+  }
+  return (
     <>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-    <TextField id="outlined-basic" label="Word to Rhyme" variant="outlined" />
-     
-    </Box>
-
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Word to Rhyme" variant="outlined" onChange={(event) => onWordToRhymeChange(event)} />
+      </Box>
     </>
-)
+  )
 }
 export default WordToRhyme;
